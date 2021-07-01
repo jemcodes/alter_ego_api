e281702e2dee844c11409ccaf10c3d1603f1babd
jemcodes <martin.jane.ellis@gmail.com>
2021-07-01 09:25:25 -0700

    Change seed data for refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Animal.create(real_name: "Stringray", alter_ego: "Majestic Sea Flap Flap", environment: "Ocean")
-Animal.create(real_name: "Bear", alter_ego: "Danger Floof", environment: "Woodlands, Prairie")
-Animal.create(real_name: "Snake", alter_ego: "Danger Noodle", environment: "Forest, Desert, Swamp, Grasslands")
-Animal.create(real_name: "Llama", alter_ego: "Giraffe Sheep", environment: "Andean highlands")
-Animal.create(real_name: "Seagull", alter_ego: "Beach Chicken", environment: "Coastal regions")
+Animal.create(real_name: "Stringray", alter_ego: "Majestic Sea Flap Flap", environment: "Sea")
+Animal.create(real_name: "Bear", alter_ego: "Danger Floof", environment: "Land")
+Animal.create(real_name: "Snake", alter_ego: "Danger Noodle", environment: "Land")
+Animal.create(real_name: "Llama", alter_ego: "Giraffe Sheep", environment: "Land")
+Animal.create(real_name: "Seagull", alter_ego: "Beach Chicken", environment: "Sky")
